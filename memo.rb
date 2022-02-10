07b72db972bcf8d7c7c54b2544433f1fb12603b4
akqbi168 <akqbi168@localhost.localdomain>
2022-02-10 22:19:46 +0900

    改行に対応。Ctrl+Dで終了可に

--- memo.rb
+++ memo.rb
@@ -7,29 +7,37 @@ if memo_type == "1"
   puts "拡張子を除いたファイル名を入力してください"
   file_name = gets.chomp
 
-  if File.exist?("#{file_name}.csv")
+  while File.exist?("#{file_name}.csv")
     puts "ファイルが存在します"
-  else
-    puts "メモしたい内容を入力してください"
-    puts "完了したら Ctrl + D をおします"
-    body = gets.to_s
-
-    CSV.open("#{file_name}.csv", "w") do |test|
-      test << [body]
-    end
-
-    puts "新規メモが保存されました"
-    puts "ファイル：#{file_name}.csv"
-    puts "保存内容：" + body[0, 50]
+    puts "拡張子を除いたファイル名を入力してください"
+    file_name = gets.chomp
   end
+
+  puts "メモしたい内容を入力してください"
+  puts "完了したら Ctrl + D をおします"
+  body = STDIN.read
+
+  CSV.open("#{file_name}.csv", "w") do |test|
+    test << [body]
+  end
+
+  puts "新規メモが保存されました"
+  puts "ファイル：#{file_name}.csv"
+  puts "保存内容：" + body[0, 50]
 
 elsif memo_type == "2"
   puts "拡張子を除いたファイル名を入力してください"
   file_name = gets.chomp
 
+  until File.exist?("#{file_name}.csv")
+    puts "ファイルが見つかりません"
+    puts "拡張子を除いたファイル名を入力してください"
+    file_name = gets.chomp
+  end
+
   puts "追記したい内容を入力してください"
   puts "完了したら Ctrl + D をおします"
-  body = gets.to_s
+  body = STDIN.read
 
   CSV.open("#{file_name}.csv", "a") do |test|
     test << [body]
@@ -38,6 +46,7 @@ elsif memo_type == "2"
   puts "メモに内容が追記されました"
   puts "ファイル：#{file_name}.csv"
   puts "追記内容：" + body[0, 50]
+
 else
   puts "アプリを再起動してください"
 
